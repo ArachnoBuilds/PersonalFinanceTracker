d3d11ce6b7c994e48d93bd84311900bbab206a11
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-11-11 11:36:07 +0530

    removed VM

--- Shared/ViewModels/MonthlyBudgetSummary.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ViewModels;
-
-public record MonthlyBudgetSummary(
-    double TotalIncome,
-    double TotalExpenses,
-    double TotalSavings)
-{
-    public double Balance => TotalIncome - (TotalExpenses + TotalSavings);
-}
