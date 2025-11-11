50fce4a73023e0e978e7a9f0c6544a97c51b078b
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-11-11 21:49:06 +0530

    added server app project

---
+++ UI/WebServerApp/Program.cs
@@ -0,0 +1,40 @@
+using Components;
+using Radzen;
+using Services;
+using WebServerApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+builder.Services.AddRadzenComponents();
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<ThemeService>();
+builder.Services.AddScoped<BudgetService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(typeof(Dashboard).Assembly);
+
+app.Run();
