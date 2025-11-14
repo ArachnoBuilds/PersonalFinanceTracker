863a7ab4dafdc41d984ce8360ceddf2128fb6239
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-11-14 14:02:31 +0530

    implemented GetCategoryDescriptionHandler

---
+++ src/Application/Features/BudgetPlanning/GetCategoryDescription/GetCategoryDescriptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Shared;
+using Application.Shared.Models;
+using Application.Shared.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.BudgetPlanning.GetCategoryDescription;
+
+public class GetCategoryDescriptionHandler(ApplicationDbContext context)
+{
+    public async Task<Result<List<string>>> DoAsync(GetCategoryDescriptionQuery query, CancellationToken cancellation = default)
+    {
+        if (query.Type is BudgetType.Summary)
+            return Errors.BudgetTypeSummaryNotAllowed;
+
+        var type = query.Type.ToString();
+        List<string> descriptions;
+        try
+        {
+            descriptions = await context.Categories
+                            .AsNoTracking()
+                            .Where(p => p.Type == type)
+                            .Select(p => p.Description)
+                            .ToListAsync(cancellation)
+                            .ConfigureAwait(false);
+        }
+        catch (Exception exc)
+        {
+            return Result.Failure<List<string>>(exc);
+        }
+        return descriptions;
+    }
+}
---
+++ src/Application/Features/BudgetPlanning/GetCategoryDescription/GetCategoryDescriptionQuery.cs
@@ -0,0 +1,5 @@
+﻿using Application.Shared.Models;
+
+namespace Application.Features.BudgetPlanning.GetCategoryDescription;
+
+public record GetCategoryDescriptionQuery(BudgetType Type);
