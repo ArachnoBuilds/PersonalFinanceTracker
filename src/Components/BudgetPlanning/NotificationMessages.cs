c57574b0ea40d77a8202b8f57adb8e4bd5bf4427
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-11-14 22:16:39 +0530

    new notification message

--- src/Components/BudgetPlanning/NotificationMessages.cs
+++ src/Components/BudgetPlanning/NotificationMessages.cs
@@ -4,4 +4,5 @@ internal static class NotificationMessages
 {
     public const string BudgetCreationFailed = "Failed to create budget entry.";
     public const string BudgetCreationSuccess = "Budget entry created successfully.";
+    public const string BudgetFetchFailed = "Failed to fetch budget data.";
 }
