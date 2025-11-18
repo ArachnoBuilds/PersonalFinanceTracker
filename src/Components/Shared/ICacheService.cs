44e74672d973c7a4b5c2e4aff5aa65dd666383a2
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-11-18 22:15:32 +0530

    implemented cache service using ProtectedBrowserStorage

---
+++ src/Components/Shared/ICacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace Components.Shared;
+
+public interface ICacheService
+{
+    // TODO check if ValueTask can be used instead
+    Task<bool> SetAsync<T>(string key, T value);
+    Task<T?> GetAsync<T>(string key);
+}
---
+++ src/WebServerApp/CacheService.cs
@@ -0,0 +1,28 @@
+﻿using Components.Shared;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace WebServerApp;
+
+public class CacheService(ProtectedLocalStorage cache) : ICacheService
+{
+    public async Task<T?> GetAsync<T>(string key)
+    {
+        var result = await cache.GetAsync<T>(key);
+        return result.Success ? result.Value : default;
+    }
+
+    public async Task<bool> SetAsync<T>(string key, T value)
+    {
+        if (value == null)
+            return false;
+        try
+        {
+            await cache.SetAsync(key, value);
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+}
