e17f86b4082fdd820c5da016fb524fd7f5ae4a44
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-11-19 21:37:03 +0530

    initiated feature Budget Tracking

---
+++ src/Components/BudgetTracking/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using Application.Shared.Models;
+
+namespace Components.BudgetTracking.Models;
+
+public record Transaction(
+    DateTime Date,
+    BudgetType BudgetType,
+    string BudgetCategory,
+    decimal Amount,
+    string Account,
+    string Description)
+{
+    public decimal Balance { get; set; } = 0.00m;
+    public DateTime EffectiveDate { get; set; } = Date;
+}
