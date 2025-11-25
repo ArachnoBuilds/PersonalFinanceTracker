80705444acdd1379e1860b9cfb93d9ccc7c932ad
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-11-25 21:19:14 +0530

    new get category in budget tracking

---
+++ src/Application/Features/BudgetTracking/Errors.cs
@@ -0,0 +1,13 @@
+﻿using Application.Shared;
+
+namespace Application.Features.BudgetTracking;
+
+internal static class Errors
+{
+    internal static Error BudgetTypeSummaryNotAllowed =>
+        new("BudgetTypeSummaryNotAllowed",
+            "The budget type 'Summary' is not allowed for this operation.");
+    internal static Error BudgetCategoryNotFound =>
+        new("BudgetCategoryNotFound",
+            "The specified budget category was not found.");
+}
---
+++ src/Application/Features/BudgetTracking/GetCategory/GetCategoryQuery.cs
@@ -0,0 +1,5 @@
+﻿using Application.Shared.Models;
+
+namespace Application.Features.BudgetTracking.GetCategory;
+
+public record GetCategoryQuery(BudgetType Type, int Year);
