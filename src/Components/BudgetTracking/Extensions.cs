b38806d1647b316602600c0dce10bd40e6308e71
Anirban Mukherjee <mukherjee.anirban@outlook.in>
2025-12-06 12:36:30 +0530

    Refactor Tracker to fetch dynamic transaction data
    Replaced hardcoded transaction data with dynamic fetching via
    `GetTransactionHandler`. Updated `@using` directives to include
    new namespaces and added `@inject` directives for the handler
    and logger services. Introduced error handling to log failures
    and notify users when fetching transactions fails. Updated the
    `data` list to dynamically populate with fetched transaction
    data, improving flexibility and robustness.

---
+++ src/Components/BudgetTracking/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using BT = Application.Features.BudgetTracking;
+
+namespace Components.BudgetTracking;
+
+internal static class Extensions
+{
+    extension(BT.Models.Transaction transaction)
+    {
+        public Models.Transaction ToTransaction() => new()
+        {
+            Id = transaction.Id,
+            Date = transaction.Date,
+            BudgetType = transaction.BudgetInfo.Type,
+            BudgetCategory = transaction.BudgetInfo.Category,
+            Account = transaction.Account,
+            Amount = transaction.Amount,
+            Description = transaction.Description,
+            EffectiveDate = transaction.EffectiveDate
+        };
+    }
+}
